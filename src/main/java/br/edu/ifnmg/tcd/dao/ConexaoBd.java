8adbf65fcfa6821c43090ee6d3354b7de70e30f0
Reinaldo Junio Dias de Abreu <reinaldodiasabreu@gmail.com>
2020-12-13 19:18:34 -0300

    Update file ConexaoBd

--- src/main/java/br/edu/ifnmg/tcd/dao/ConexaoBd.java
+++ src/main/java/br/edu/ifnmg/tcd/dao/ConexaoBd.java
@@ -1,34 +1,40 @@
 package br.edu.ifnmg.tcd.dao;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
  * Tratamento da conexão com o banco de dados.
- * 
- * @author Reinaldo Junio Dias de Abreu
- * @version 0.0.1, 13/12/2020
+ *
+ * @author Luis Guisso <luis dot guisso at ifnmg dot edu dot br>
+ * @version 0.0.1, 29/11/2020
  */
 public class ConexaoBd {
 
     /**
-     * URL de conexão com o banco de dados (protocolo/sgbd/ip/porta/banco/parâmetros).
+     * URL de conexão com o banco de dados
+     * (protocolo/sgbd/ip/porta/banco/parâmetros).
      */
     public static final String URL;
-    
+
     /**
-     * Retém a conexão estabelecida com o banco de dados durante a operação do sistema.
+     * Retém a conexão estabelecida com o banco de dados durante a operação do
+     * sistema.
      */
     private static Connection conexao;
-    
+
     /**
      * Usuário para acesso ao banco de dados.
      */
     private static String usuario;
-    
+
     /**
      * Senha para acesso ao banco de dados.
      */
@@ -38,19 +44,49 @@ public class ConexaoBd {
      * Inicialização de valores estáticos.
      */
     static {
-        //URL = "jdbc:mysql://remotemysql.com:3306/4GjflrvG29?useUnicode=true&useJDBCCompliantTimezoneShift=true&serverTimezone=UTC";
-        URL = "jdbc:mysql://remotemysql.com:3306/4GjflrvG29?zeroDateTimeBehavior=CONVERT_TO_NULL";
-        usuario = "4GjflrvG29";
-        senha = "TPzeiPav61";
-        /*URL = "jdbc:mysql://127.0.0.1:3306/TCD?useUnicode=true&useJDBCCompliantTimezoneShift=true&serverTimezone=UTC";
-        usuario = "root";
-        senha = "99049663";*/
+        if (false) {
+            // Servidor Local
+            URL = "jdbc:mysql://127.0.0.1:3306/tarefas"
+                    + "?useUnicode=true"
+                    + "&useJDBCCompliantTimezoneShift=true"
+                    + "&serverTimezone=UTC"
+                    + "&autoReconnect=true";
+            usuario = "root";
+            senha = "";
+
+        } else {
+            // Servidor remoto
+            Properties prop = new Properties();
+            String propFileName = "bd.properties";
+
+            InputStream inputStream = ConexaoBd.class.getClassLoader()
+                    .getResourceAsStream(propFileName);
+
+            if (inputStream != null) {
+                try {
+                    prop.load(inputStream);
+                } catch (IOException ex) {
+                    Logger.getLogger(ConexaoBd.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            } else {
+                System.out.println("Arquivo de propriedades '" + propFileName
+                        + "' não encontrado no classpath");
+                System.exit(0);
+            }
+
+            URL = prop.getProperty("url");
+            usuario = prop.getProperty("user");
+            senha = prop.getProperty("password");
+
+        }
+
     }
 
     //<editor-fold defaultstate="collapsed" desc="Construtor privado">
     /**
      * Construtor privado para forçar acesso à conexão pelo membro estático
-     * <code>getConexao()</code> sem que sejam gerados novos objetos "ConexaoBd".
+     * <code>getConexao()</code> sem que sejam gerados novos objetos
+     * "ConexaoBd".
      */
     private ConexaoBd() {
     }
@@ -76,7 +112,7 @@ public class ConexaoBd {
 
     /**
      * Estabele e gera a retenção da conexão com o banco de dados.
-     * 
+     *
      * @return Conexão com o banco de dados.
      */
     public static Connection getConexao() {
@@ -85,18 +121,17 @@ public class ConexaoBd {
         if (conexao == null) {
             // ... tenta ...
             try {
-                
+                System.out.println(">> Nova conexão estabelecida com o banco de dados");
                 // ... estabelecer e reter a conexão a partir da URL, 
                 // do usuário e da senha fornecidos
                 conexao = DriverManager.getConnection(URL, usuario, senha);
-                System.out.println(">> Nova conexão estabelecida com o banco de dados");
             } catch (SQLException ex) {
                 // TODO Rever procedimento e encerrar o programa em caso de falha
                 // Caso ocorra alguma falha, registra falha
                 Logger.getLogger(ConexaoBd.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-        
+
         // Devolve a conexão estabelecida
         return conexao;
 
